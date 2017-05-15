665b047e605d25191c5bfb4bd470689b279bea94
CrazzyHuang <506605454@qq.com>
2017-05-16 00:47:39 +0800

    add widgets

---
+++ js/widgets/rightArea.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Administrator on 2017/5/16 0016.
+ */
+!(function ($) {
+    "use strict";
+     var RightArea = function () {
+
+         RightArea.prototype.initStyle = function () {
+
+         };
+
+         RightArea.prototype.initEvent = function () {
+
+         };
+         
+     };
+     
+
+     window.RightArea = RightArea;
+
+})(jQurey);
