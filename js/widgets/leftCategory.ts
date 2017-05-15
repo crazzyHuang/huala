658279999cad6e5c71ba3c57904312a58923d106
CrazzyHuang <506605454@qq.com>
2017-05-15 23:13:26 +0800

    remove webpack

--- js/widgets/leftCategory.ts
+++ js/widgets/leftCategory.ts
@@ -2,16 +2,20 @@
  * Created by Administrator on 2017/5/15 0015.
  */
 import  * as $ from "jquery"
- class LeftCategory{
-    initEvent(){
+class LeftCategory {
 
-        $("#login").on("click",function () {
-            alert(1);
+    initEvent() {
+        $(".goods-category-detail li").hover(function (event: JQueryEventObject) {
+            var onObj = $(this).addClass("goods-category-detail-on");
+            var offObj =onObj.siblings().removeClass("goods-category-detail-on");
+            onObj.find('a').addClass("goods-category-detail-a-on");
+            offObj.find('a').removeClass("goods-category-detail-a-on");
+
+
+            // goods-category-detail li:hover > a
+
+
         })
-
-        // $(".goods-category-detail li").hover(function (e) {
-        //     $(this).addClass("goods-category-detail");
-        // })
     }
 }
 
